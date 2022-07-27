067dac9983477e26e24fc3adab0e015e00150788
minhcuong2102 <cuong21022003@gmail.com>
2022-07-27 17:45:22 +0700

    nopbai

--- bai11/luyen_tap_vong_lap_01/luyen_tap_vong_lap_01.js
+++ bai11/luyen_tap_vong_lap_01/luyen_tap_vong_lap_01.js
@@ -39,22 +39,76 @@ function bai4() {
         }
         return fibonacci(n-1) + fibonacci(n-2);
     }
-
-
+    let i = 1;
+    while (fibonacci(i)){
+        if(fibonacci(i) % 5 === 0){
+            document.write(fibonacci(i));
+            break;
+        } else {
+            i++;
+        }
+    }
 }
 
 function bai5() {
-
+    function fibonacci(n) {
+        if (n === 1 || n === 2){
+            return 1;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    let tong = 0;
+    for (let i = 1; i <= 20; i++){
+        tong += fibonacci(i);
+    }
+    alert(tong);
 }
 
 function bai6() {
-
+    let tong = 0;
+    let cnt = 0;
+    while (cnt < 30) {
+        for (let i = 0; i <= 30; i++) {
+            if (i % 7 === 0) {
+                tong += i;
+            }
+        }
+        cnt++;
+    }
+    alert(tong);
 }
 
 function bai7() {
-
+    for (let i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+            document.write("FizzBuzz" + '<br>');
+        }else {
+            if (i % 3 === 0) {
+            document.write("Fizz" + '<br>');
+            } else if (i % 5 === 0) {
+            document.write("Buzz" + '<br>');
+        }
+        else {
+            document.write(i + '<br>');
+            }
+        }
+    }
 }
 
 function bai8() {
-
+    let khoangSo = Number(prompt("Nhập khoảng số muốn đoán: "));
+    let nhapSo = Number(prompt("Nhập số: "));
+    let soNgauNhien;
+    if(khoangSo){
+        soNgauNhien = Math.floor(Math.random() * khoangSo);
+        if(soNgauNhien === nhapSo){
+            alert("Bạn đã đoán đúng!");
+        }else {
+            while (soNgauNhien !== nhapSo){
+                alert("Bạn đã đoán sai, số chính xác là: " + soNgauNhien);
+                nhapSo = Number(prompt("Nhập số lại: "))
+            }
+            alert("Bạn đã đoán đúng!");
+        }
+    }
 }
