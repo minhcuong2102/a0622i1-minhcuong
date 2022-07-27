3cb8ddb13d92087635701c2ce424ea4b3cea3346
minhcuong2102 <cuong21022003@gmail.com>
2022-07-27 20:20:36 +0700

    nopbai

--- bai11/luyen_tap_vong_lap_02/luyen_tap_vong_lap_02.js
+++ bai11/luyen_tap_vong_lap_02/luyen_tap_vong_lap_02.js
@@ -25,17 +25,103 @@ function bai_2() {
 }
 
 function bai_3() {
-    
+    let chieuCao = Number(prompt("Nhập chiêu cao: "));
+    while (chieuCao <= 0){
+        alert("Số phải là nguyên dương!");
+        chieuCao = Number(prompt("Nhập lại chiều cao: "));
+    }
+    let kieuDang = Number(prompt("Nhập kiểu dáng: 1. Vuông góc dưới bên trái, 2. Vuông góc trên bên trái, 3. Vuông góc dưới bên phải, 4. Vuông góc trên bên phải."));
+    switch (kieuDang) {
+        case 1:
+            for (let i = 1; i <= chieuCao; i++){
+                    for (j = 1; j <= i; j++){
+                        document.write(' * ');
+                    }
+                    document.write("<br>");
+                }
+            break;
+
+        case 2:
+            for (let i = chieuCao; i >= 1; i--){
+                for (let j = 1; j <= i; j++){
+                    document.write(" * ")
+                }
+                document.write("<br>");
+            }
+            break;
+
+        case 3:
+            for (let i = 1; i <= chieuCao; i++){
+                    for (let j = 1; j <= chieuCao; j++){
+                        if (j <= chieuCao - i){
+                            document.write("&nbsp&nbsp");
+                        }else {
+                            document.write("*");
+                        }
+                    }
+                    document.write("<br>");
+                }
+            break;
+
+        case 4:
+            for (let i = chieuCao; i >= 1; i--){
+                for (let j = 1; j <= chieuCao; j++){
+                    if (j <= chieuCao - i){
+                        document.write("&nbsp&nbsp");
+                    }else {
+                        document.write("*")
+                    }
+                }
+                document.write('<br>');
+            }
+            break;
+    }
 }
 
 function bai_4() {
-
+    let chieuDai = Number(prompt("Nhập chiều dài: "));
+    let chieuRong = Number(prompt("Nhập chiều rộng: "));
+    while (chieuDai <= 0 || chieuRong <= 0){
+        alert("Chiều dài hoặc chiều rộng phải lớn hơn 0!");
+        chieuDai = Number(prompt("Nhập lại chiều dài: "));
+        chieuRong = Number(prompt("Nhập lại chiều rộng: "));
+    }
+    for (let i = 1; i <= chieuRong; i++){
+        for (let j = 1; j <= chieuDai; j++){
+            if (i === 1 || j === 1 || i === chieuRong || j === chieuDai){
+                document.write("*");
+            }else {
+                document.write("&nbsp&nbsp");
+            }
+        }
+        document.write("<br>");
+    }
 }
 
 function bai_5() {
-
+    let tienVon = parseFloat(prompt("Nhập số tiền vốn: "));
+    let soThang = parseFloat(prompt("Nhập số tháng cho vay: "));
+    let laiSuat = parseFloat(prompt("Nhập lãi suất hàng tháng: "));
+    let tongTien = tienVon;
+    for (let i = 1; i <= soThang; i++){
+        tongTien *= (1 + laiSuat);
+    }
+    let tienLai = tongTien - tienVon;
+    alert("Số tiền lãi là: " + tienLai.toFixed(0));
 }
 
 function bai_6() {
-
+    for (let i = 1; i < 7; i++){
+        for (let j = 1; j <= 7; j++){
+            if (!((i === 1 &&(j === 1 || j === 4 || j === 7))
+            || (i === 4 && (j === 1 || j === 7))
+            || (i === 5 && (j <= 2 || j >= 6))
+            || (i === 6 && (j <= 3 || j >= 5)))){
+                document.write(" * ");
+            }else {
+                document.write("&nbsp&nbsp");
+            }
+        }
+        document.write("<br>");
+    }
 }
