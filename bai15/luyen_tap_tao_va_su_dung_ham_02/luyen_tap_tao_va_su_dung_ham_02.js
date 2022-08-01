07af4c6ccca871c4ae2d343c266b42dbdebf2f1b
minhcuong2102 <cuong21022003@gmail.com>
2022-08-01 21:19:52 +0700

    update

---
+++ bai15/luyen_tap_tao_va_su_dung_ham_02/luyen_tap_tao_va_su_dung_ham_02.js
@@ -0,0 +1,39 @@
+function bai01() {
+    function binhPhuong(a) {
+        return a * a;
+    }
+    let num = parseFloat(prompt("Nhập số: "));
+    alert("Bình phương của số " + num + " là: " + binhPhuong(num));
+}
+
+function bai02() {
+    
+}
+
+function bai03() {
+
+}
+
+function bai04() {
+
+}
+
+function bai05() {
+
+}
+
+function bai06() {
+
+}
+
+function bai07() {
+
+}
+
+function bai08() {
+
+}
+
+function bai09() {
+
+}
