e505f35d8d51cf319ff187608b7b2fb6e8deb98f
minhcuong2102 <cuong21022003@gmail.com>
2022-08-01 21:06:46 +0700

    nopbai

---
+++ bai15/luyen_tap_tao_va_su_dung_ham_01/luyen_tap_tao_va_su_dung_ham_01.js
@@ -0,0 +1,53 @@
+function bai1() {
+    alert("Xin chao");
+}
+
+function bai2() {
+    function tangDonVi(n) {
+        return ++n;
+    }
+    let num = Number(prompt("Nhập số: "));
+    let num1 = tangDonVi(num);
+    alert("Số mới là: " + num1);
+}
+
+function bai3() {
+    function thamSo(a, b){
+        a = Number(prompt("Nhập số a: "));
+        b = Number(prompt("Nhập số b: "));
+        if (a > b) {
+            alert("a lớn hơn b!");
+        } else {
+            return a + b;
+        }
+    }
+    console.log(thamSo());
+}
+
+function bai4() {
+    function addNumbers(firstNum, secondNum, result) {
+        firstNum = 4;
+        secondNum = 8;
+        result = firstNum + secondNum;
+        return result;
+    }
+    let result = 0;
+    alert("Giá trị cũ là: " + result);
+    result = addNumbers();
+    alert("Giá trị mới là: " + result);
+}
+
+function bai5() {
+    function chomSao(ngoiSao){
+            if (arr1.includes(ngoiSao)){
+                let index = arr1.indexOf(ngoiSao)
+                alert("Ngôi sao " + ngoiSao + " thuộc chòm sao " + arr2[index]);
+            }else {
+                alert("Không tìm thấy ngôi sao nào!");
+            }
+        }
+    let arr1 = ["Polaris", "Aldebaran", "Deneb", "Vega", "Altair", "Dubhe", "Regulus"];
+    let arr2 = ["Ulsa Minor", "Tarurus", "Cygnus", "Lyra", "Aquila", "Ulsa Major", "Leo"];
+    let ngoiSao = prompt("Nhập tên ngôi sao: ");
+    chomSao(ngoiSao);
+}
