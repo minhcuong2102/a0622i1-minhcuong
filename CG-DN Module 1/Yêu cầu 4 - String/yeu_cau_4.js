5d5a8f64f5cae2468cfc2e02062cbe5ca675359e
minhcuong2102 <cuong21022003@gmail.com>
2022-08-26 18:38:01 +0700

    nopbai

--- CG-DN Module 1/Yêu cầu 4 - String/yeu_cau_4.js
+++ CG-DN Module 1/Yêu cầu 4 - String/yeu_cau_4.js
@@ -3,16 +3,16 @@ function bai4_1() {
         let arr1 = str.split(" ");
         let cnt = 0;
         for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] === " "){
+            if (arr1[i] === " ") {
                 for (let j = i; j < arr1.length; j++) {
-                    if (arr1[j] === " "){
+                    if (arr1[j] === " ") {
                         cnt++;
-                    }else {
+                    } else {
                         break;
                     }
                 }
             }
-            if (cnt !== 0){
+            if (cnt !== 0) {
                 arr1.splice(i, cnt);
                 cnt = 0;
             }
@@ -26,11 +26,11 @@ function bai4_1() {
         });
     }
 
-    function findLongestWord(mang){
+    function findLongestWord(mang) {
         let max = mang[0].length;
         let cnt = 0;
         for (let i = 1; i < mang.length; i++) {
-            if (max < mang[i].length){
+            if (max < mang[i].length) {
                 max = mang[i].length;
                 cnt = i;
             }
@@ -48,21 +48,21 @@ function bai4_1() {
 }
 
 function bai4_2() {
-    function differentSymbolsNaive(str){
+    function differentSymbolsNaive(str) {
         let arr1 = str.split("");
         let arr2 = [];
         let flag = true;
         arr2[0] = arr1[0];
         for (let i = 0; i < arr1.length; i++) {
             for (let j = 0; j < arr2.length; j++) {
-                if (arr1[i] === arr2[j]){
+                if (arr1[i] === arr2[j]) {
                     flag = false;
                     break;
-                }else {
+                } else {
                     flag = true;
                 }
             }
-            if (flag){
+            if (flag) {
                 arr2.push(arr1[i]);
             }
         }
@@ -74,30 +74,57 @@ function bai4_2() {
 }
 
 function bai4_3() {
-    function commonCharacterCount(str1, str2){
-        let arr1 = str1.split("");
-        let arr2 = str2.split("");
-        console.log(arr1, arr2);
-        let arr3 = [];
-        for (let i = 0; i < arr1.length; i++) {
-            for (let j = 0; j < arr2.length; j++) {
-                if (arr1[i] === arr2[j]){
-                    let temp = arr1[i];
-                    let a = ++i;
-                    let b = ++j;
-                    while (arr1[a] === arr2[b]){
-                        temp += arr1[a];
-                        a++; b++; i++; j++;
-                    }
-                    arr3.push(temp);
-                }
+    function ghepSoLienKe(arr) {
+        let tmp = [];
+        for (let i = 0; i < arr.length; i++) {
+            let str = arr[i];
+            while (arr[i] == arr[i + 1]) {
+                str += arr[i + 1];
+                i++;
             }
+            tmp.push(str);
         }
-        console.log("Các kí tự giống nhau là: " + arr3);
-        return arr3.length;
+        return tmp;
     }
 
-    let str1 = prompt("Nhập chuỗi thứ nhất: ")
-    let str2 = prompt("Nhập chuỗi thứ hai: ")
-    alert("Số kí tự giống nhau là: " + commonCharacterCount(str1, str2));
+    function commonCharacterCount(str, arr, arr3) {
+        let mangTach = [];
+        for (let i = 0; i < str.length; i++) {
+            mangTach.push(str.substr(i));
+        }
+        loop1:
+            for (let i = 0; i < mangTach.length; i++) {
+                loop2:
+                    for (let j = 0; j < arr.length; j++) {
+                        if (mangTach[i] == arr[j]) {
+                            arr3.push(mangTach[i]);
+                            break loop1;
+                        }
+                    }
+            }
+    }
+
+    let str1 = prompt("Nhập chuỗi thứ nhất: ");
+    let str2 = prompt("Nhập chuỗi thứ hai: ");
+    let arr1 = ghepSoLienKe(str1);
+    let arr2 = ghepSoLienKe(str2);
+    let arr3 = [];
+    for (let i = 0; i < arr1.length; i++) {             // Kiểm tra phần tử chung mảng 1 so với mảng 2 rồi thêm các PT chung vào mảng 3
+        commonCharacterCount(arr1[i], arr2, arr3);
+    }
+
+    for (let j = 0; j < arr2.length; j++) {             // Kiểm tra phần tử chung mảng 2 so với mảng 1 rồi thêm các PT chung vào mảng 3
+        commonCharacterCount(arr2[j], arr1, arr3);
+    }
+
+    for (let k = 0; k < arr3.length; k++) {            // Loại bỏ các phần tử chung trong mảng 3
+        for (let h = k + 1; h < arr3.length; h++) {
+            if (arr3[k] == arr3[h]) {
+                arr3.splice(k, 1);
+                k--;
+                break;
+            }
+        }
+    }
+    alert(arr3);
 }
