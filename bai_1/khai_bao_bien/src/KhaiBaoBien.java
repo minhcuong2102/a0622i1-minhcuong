a37155115c439476e016db83af7a66bd38549433
minhcuong2102 <cuong21022003@gmail.com>
2022-09-05 18:19:41 +0700

    nopbai

---
+++ bai_1/khai_bao_bien/src/KhaiBaoBien.java
@@ -0,0 +1,12 @@
+public class KhaiBaoBien {
+    public static void main(String[] args) {
+        int i = 0;
+        char c = 'c';
+        String s = "Codegym";
+        float f = 20.5f;
+        System.out.println("i = " + i);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+        System.out.println("f = " + f);
+    }
+}
