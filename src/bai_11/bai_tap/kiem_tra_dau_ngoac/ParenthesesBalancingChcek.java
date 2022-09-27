6e303dea804fcbef801bb619585c9ec97713a99f
minhcuong2102 <cuong21022003@gmail.com>
2022-09-27 19:24:48 +0700

    nopbai

---
+++ src/bai_11/bai_tap/kiem_tra_dau_ngoac/ParenthesesBalancingChcek.java
@@ -0,0 +1,34 @@
+package bai_11.bai_tap.kiem_tra_dau_ngoac;
+
+import java.util.*;
+
+public class ParenthesesBalancingChcek {
+    public static boolean Parentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(Parentheses(input));
+    }
+}
